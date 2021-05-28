31d2c99615647444d32333184e5917ce050ae6dd
tikoStudio <timothy.koenig@mail.com>
2021-05-28 11:03:00 +0200

    component: task added

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,8 +1,11 @@
-const Tasks = ({tasks}) => {
+import Task from './Task'
+
+const Tasks = ({ tasks, onDelete }) => {
   return (
       <>
-        {tasks.map((task) => (<h3 key={task.id}>{task.text}</h3>)
-        )}
+        {tasks.map((task) => (
+          <Task key={task.id} task={task} onDelete={onDelete} />
+        ))}
       </>
   )
 }
